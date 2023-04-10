717298cc57d4acae32cef0b1bd9fbaaa7b9398c6
“Jamie <jamiedevine77@gmail.com>
2023-04-10 15:47:03 -0500

    workshopcomplete

--- routes/campsiteRouter.js
+++ routes/campsiteRouter.js
@@ -107,7 +107,7 @@ campsiteRouter.route('/:campsiteId/comments')
     Campsite.findById(req.params.campsiteId)
     .then(campsite => {
         if (campsite) {
-        req.body.author = req.user_id;
+        req.body.author = req.user._id;
         campsite.comments.push(req.body);
         campsite.save()
         .then(campsite => {
@@ -182,21 +182,27 @@ campsiteRouter.route('/:campsiteId/comments/:commentId')
 
 .put(authenticate.verifyUser, (req, res, next) => {
     Campsite.findById(req.params.campsiteId)
-    .then(campsite => {
-        if (campsite && campsite.comments.id(req.params.commentId)) {
+    .then((campsite) => {
+    if (campsite && campsite.comments.id(req.params.commentId)) {
+        if((campsite.comments.id(req.params.commentId).author._id).equals(req.user._id)) {
             if(req.body.rating) {
                 campsite.comments.id(req.params.commentId).rating = req.body.rating;
             }
             if (req.body.text) {
-                campsite.comments.id(req.params.commentId).author._id.equals(req.user._id)
+                campsite.comments.id(req.params.commentId).text = req.body.text;
             }
             campsite.save()
-            .then(campsite => {
+            .then((campsite) => {
                 res.statusCode = 200;
                 res.setHeader('Content-Type', 'application/json');
                 res.json(campsite)
             })
             .catch(err => next(err));
+        } else {
+            const err = new Error('You are not authorized to update this comment!');
+            err.status = 403;
+            return next(err);
+        }
     } else if (!campsite) {
         err = new Error(`Campsite ${req.params.campsiteId} not found`);
         err.status = 404;
@@ -208,6 +214,7 @@ campsiteRouter.route('/:campsiteId/comments/:commentId')
         }
     })
     .catch(err => next(err)); 
+    
 })
 
 .delete(authenticate.verifyUser, (req, res, next) => {
